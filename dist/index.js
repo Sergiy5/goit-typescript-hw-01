63fe9ddb0736a60d361dc292592568e42c0c2a49
Sergiy Kushnir <s1983kushnir@gmail.com>
2023-10-17 21:12:56 +0300

    Fake

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,7 @@
-import { concatenation } from './concatenation';
+// import { concatenation } from './concatenation';
+function concatenation(firstWord, secondWord) {
+  console.log(`${firstWord} ${secondWord}`);
+}
 const button = document.querySelector('button');
 const input = document.querySelector('input');
 if (button && input) {
